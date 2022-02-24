51cdccefe80d9c222c66acac53c08333bd3e6bb3
chudanghieu <kunzhieu1999@gmail.com>
2022-02-24 10:00:44 -0500

    UI change

--- app/src/main/java/ca/cheems/iotproject/MainActivity.java
+++ app/src/main/java/ca/cheems/iotproject/MainActivity.java
@@ -5,8 +5,20 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -18,7 +30,9 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,12 +40,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Button pm25, tvoc, eCO2;
     ImageView face, temp_pic;
-    TextView temperature, humid;
+    TextView temperature, humid,temp_ban, humid_ban, location;
     double temp, humd, voc, pm, co2;
+    LinearLayout top;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +58,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        getID();
+       buttonPress();
 
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Sensorvalue");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                temp = dataSnapshot.child("temp").getValue(double.class);
+                humd = dataSnapshot.child("humid").getValue(double.class);
+                voc = dataSnapshot.child("tvoc").getValue(double.class);
+                pm = dataSnapshot.child("pm25").getValue(double.class);
+                co2 = dataSnapshot.child("eco2").getValue(double.class);
+
+                aq_levels_indicators();
+                setTextOnScreen();
+                changeTempPic();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+    }
+
+
+    public void buttonPress(){
         pm25.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                openPM25activity();
+            public void onClick(View view) { openPM25activity();
             }
         });
 
@@ -60,34 +103,6 @@ public class MainActivity extends AppCompatActivity {
                 openeCO2activity();
             }
         });
-
-
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Sensorvalue");
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                temp = dataSnapshot.child("temp").getValue(double.class);
-                humd = dataSnapshot.child("humid").getValue(double.class);
-                voc = dataSnapshot.child("tvoc").getValue(double.class);
-                pm = dataSnapshot.child("pm25").getValue(double.class);
-                co2 = dataSnapshot.child("eco2").getValue(double.class);
-
-                aq_levels_indicators();
-                setTextOnScreen();
-                changeTempPic();
-
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
     }
 
     public void changeTempPic(){
@@ -106,34 +121,60 @@ public class MainActivity extends AppCompatActivity {
         temp_pic = (ImageView) findViewById(R.id.temp_pic);
 
         //The face imageview will change according to the level of
-        face = findViewById(R.id.face_indicator);
-        temperature = findViewById(R.id.aq_temp);
-        humid = findViewById(R.id.aq_humd);
+        face =(ImageView) findViewById(R.id.face_indicator);
+        //temp and humidity
+        temperature =(TextView) findViewById(R.id.aq_temp);
+        humid = (TextView)  findViewById(R.id.aq_humd);
+        top = (LinearLayout) findViewById(R.id.aqilayout);
+        temp_ban = (TextView) findViewById(R.id.aq_temp_banner);
+        humid_ban = (TextView) findViewById(R.id.aq_humd_banner);
+
     }
 
     public void setTextOnScreen(){
         temperature.setText(String.valueOf(temp) +" °C");
         humid.setText(String.valueOf(humd) + " %");
-        tvoc.setText(String.valueOf(voc) + " ppb");
-        pm25.setText(String.valueOf(pm) + " μg/m3");
-        eCO2.setText(String.valueOf(co2) + " ppm");
+        tvoc.setText("tVOC = "+ String.valueOf(voc) + " ppb");
+        pm25.setText("PM2.5 = " +String.valueOf(pm) + " μg/m3");
+        eCO2.setText("eCO2 = " +String.valueOf(co2) + " ppm");
     }
 
     public void aq_levels_indicators(){
         //Change face due to the air quality condition
                 if (voc <= 250){
-                    face.setImageResource(R.drawable.happy);
+                   happy();
                 }else if(voc > 250 && voc <= 2000){
-                    face.setImageResource(R.drawable.worry);
+                    worry();
                 }else{
-                    face.setImageResource(R.drawable.angry);
+                    angry();
                 }
+    }
 
+    public void happy(){
+        face.setImageResource(R.drawable.happy);
+        top.setBackground(getDrawable(R.drawable.background_top_good));
+        temp_ban.setTextColor(Color.parseColor("#000000"));
+        humid_ban.setTextColor(Color.parseColor("#000000"));
+    }
+
+    public void worry(){
+        face.setImageResource(R.drawable.worry);
+        top.setBackground(getDrawable(R.drawable.background_top_medium));
+        temp_ban.setTextColor(Color.parseColor("#E6EDf5"));
+        humid_ban.setTextColor(Color.parseColor("#E6EDf5"));
+    }
+
+    public void angry(){
+        face.setImageResource(R.drawable.angry);
+        top.setBackground(getDrawable(R.drawable.background_top_bad));
+        temp_ban.setTextColor(Color.parseColor("#FFFFFF"));
+        humid_ban.setTextColor(Color.parseColor("#FFFFFF"));
     }
 
     public void openeCO2activity(){
         Intent intent = new Intent(this, eCO2Activity.class);
         startActivity(intent);
+
     }
 
     public void openTVOCactivity() {
@@ -145,10 +186,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, pm25Activity.class);
         startActivity(intent);
     }
-
-
-
-
 
     @Override
     public boolean onKeyLongPress(int keyCode, KeyEvent event)
